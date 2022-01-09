e37ed4ade02195031b6bff228f9ec1b268597e14
zsofi <zsofiath@gmail.com>
2022-01-09 19:34:07 +0100

    :tada: init food order excercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './user-search/App';
+import App from './food-order/App';
 
 ReactDOM.render(<App />,
   document.getElementById('root')
