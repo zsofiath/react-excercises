c16b6b7ebe5c33f0c0c5cfcb4e5878b62abe81c7
zsofi <zsofiath@gmail.com>
2022-01-10 16:13:41 +0100

    :sparkles: adding a cart context

--- src/food-order/App.js
+++ src/food-order/App.js
@@ -3,14 +3,15 @@ import Cart from "./components/Chart/Cart";
 import Header from "./components/Layouts/Header";
 import Meals from "./components/Meal/Meals";
 import "./index.css";
+import CartProvider from "./store/Cart-provider";
 
 export default function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
