70cbb27cc9d7fc93f3f813f4534319df2dcd7846
zsofi <zsofiath@gmail.com>
2022-01-11 09:04:17 +0100

    :sparkles: forvard input ref

--- src/food-order/components/Meal/MealItem/MealItemForm.js
+++ src/food-order/components/Meal/MealItem/MealItemForm.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import Input from '../../UI-elements/Input';
+import React, {useRef} from "react";
+import Input from "../../UI-elements/Input";
 import classes from "./MealItemForm.module.css";
 
 const Mealitemform = (props) => {
-    return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount_'+props.id,
-                type: 'number',
-                min: 1,
-                max: 5,
-                step: 1,
-                defaultValue: 1
-            }}/>
-            <button>+ Add</button>
-        </form>
-    );
-}
+    const inputAmountRef = useRef();
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        ref={inputAmountRef}
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default Mealitemform;
