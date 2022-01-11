1dec754b061f2dda03a8fe5046980979ab1fd8aa
zsofi <zsofiath@gmail.com>
2022-01-11 08:58:59 +0100

    :sparkles: adding a cart reducer

--- src/food-order/components/Layouts/HeaderChartButon.js
+++ src/food-order/components/Layouts/HeaderChartButon.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
+import CartContext from "../../store/Cart-context";
 const HeaderChartButton = ({onClick}) => {
+  const context = useContext(CartContext);
+  const numberOfChartItems = context.items.reduce((prev, item) => item.amount+prev, 0)
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>7</span>
+      <span className={classes.badge}>{numberOfChartItems}</span>
     </button>
   );
 };
--- src/food-order/store/Cart-provider.js
+++ src/food-order/store/Cart-provider.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import CartContext from './Cart-context';
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            const updatesItems = state.items.concat(action.item)
+            const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+            return {
+                items: updatesItems,
+                totalAmount: updatedTotalAmount
+            }
+    
+        default:
+            return defaultCartState;
+
+    }
+}
+
 const CartProvider = (props) => {
-    const addItemToCartHandler = item => {}
-    const removeItemFromCartHandler = item => {}
+    const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = item => {
+        dispatchCart({type: 'ADD', item});
+    }
+    const removeItemFromCartHandler = id => {
+        dispatchCart({type: 'REMOVE', id});
+    }
     const cartContext = {
         items:[],
         totalAmount: 0,
