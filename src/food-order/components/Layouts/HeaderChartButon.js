41cff1c16507e2c9fbb34173611c0fc2a14cfaa0
zsofi <zsofiath@gmail.com>
2022-01-12 15:10:34 +0100

    :sparkles: adding button pump using useEffect

--- src/food-order/components/Layouts/HeaderChartButon.js
+++ src/food-order/components/Layouts/HeaderChartButon.js
@@ -1,12 +1,33 @@
-import React, {useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
 import CartContext from "../../store/Cart-context";
-const HeaderChartButton = ({onClick}) => {
+const HeaderChartButton = ({ onClick }) => {
   const context = useContext(CartContext);
-  const numberOfChartItems = context.items.reduce((prev, item) => item.amount+prev, 0)
+  const [isButtonHighlighted, setisButtonHighlighted] = useState(false);
+  const numberOfChartItems = context.items.reduce(
+    (prev, item) => item.amount + prev,
+    0
+  );
+  const buttonClasses = `${classes.button} ${
+    isButtonHighlighted ? classes.bump : ""
+  }`;
+
+  const { items } = context;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setisButtonHighlighted(true);
+    const timer = setTimeout(() => {
+      setisButtonHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
