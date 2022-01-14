dee3224b50c64f8acc8df014870b4245e6be1eb5
zsofi <zsofiath@gmail.com>
2022-01-14 18:46:25 +0100

    :sparkles: required validation on submit

--- src/forms/components/SimpleInput.js
+++ src/forms/components/SimpleInput.js
@@ -3,6 +3,7 @@ import {useRef, useState} from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef(); // better if needed once (login)
   const [enteredName, setEnteredName] = useState(''); //better for instant validation, resetting
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -14,12 +15,22 @@ const SimpleInput = (props) => {
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     // nameInputRef.current.value <-- direct dom manipulation. !!! avoid it !!!
+
+    if(enteredName.trim()==='') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+    console.log('Boom');
   }
+
+  const nameInputClasses = `form-control${enteredNameIsValid ? '' : ' invalid'}`;
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} onChange={nameChangeHandler} type='text' id='name' />
+        {!enteredNameIsValid && <p className='error-text'>Entered name is not valid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
