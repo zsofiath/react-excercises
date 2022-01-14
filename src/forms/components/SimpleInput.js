c6d30c570cf7e6dd8b73ae3388f70e2f6693bd3f
zsofi <zsofiath@gmail.com>
2022-01-14 18:58:08 +0100

    :sparkles: set touched initially setting is valid to true is a LIE!!!

--- src/forms/components/SimpleInput.js
+++ src/forms/components/SimpleInput.js
@@ -3,7 +3,8 @@ import {useRef, useState} from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef(); // better if needed once (login)
   const [enteredName, setEnteredName] = useState(''); //better for instant validation, resetting
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
 
   const nameChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -16,6 +17,8 @@ const SimpleInput = (props) => {
     const enteredValue = nameInputRef.current.value;
     // nameInputRef.current.value <-- direct dom manipulation. !!! avoid it !!!
 
+    setEnteredNameIsTouched(true);
+    
     if(enteredName.trim()==='') {
       setEnteredNameIsValid(false);
       return;
@@ -24,13 +27,14 @@ const SimpleInput = (props) => {
     console.log('Boom');
   }
 
-  const nameInputClasses = `form-control${enteredNameIsValid ? '' : ' invalid'}`;
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+  const nameInputClasses = `form-control${!nameInputIsInvalid ? '' : ' invalid'}`;
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} onChange={nameChangeHandler} type='text' id='name' />
-        {!enteredNameIsValid && <p className='error-text'>Entered name is not valid</p>}
+        {nameInputIsInvalid && <p className='error-text'>Entered name is not valid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
