6df8e166297aba7a79ec34a521898356f10bdcdb
zsofi <zsofiath@gmail.com>
2022-01-14 19:07:31 +0100

    :sparkles: blur handler, revalidating onChange state value cannot be used because of state scedulation. it is async so the validation would handle the prewious value probably

--- src/forms/components/SimpleInput.js
+++ src/forms/components/SimpleInput.js
@@ -6,8 +6,11 @@ const SimpleInput = (props) => {
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
 
-  const nameChangeHandler = event => {
+  const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+    if(event.target.value.trim()!=='') {
+      setEnteredNameIsValid(true);
+    }
   }
 
   const formSubmissionHandler = event => {
@@ -18,7 +21,7 @@ const SimpleInput = (props) => {
     // nameInputRef.current.value <-- direct dom manipulation. !!! avoid it !!!
 
     setEnteredNameIsTouched(true);
-    
+
     if(enteredName.trim()==='') {
       setEnteredNameIsValid(false);
       return;
@@ -27,13 +30,20 @@ const SimpleInput = (props) => {
     console.log('Boom');
   }
 
+  const nameInputBlurHandler = () => {
+    setEnteredNameIsTouched(true);
+    if(enteredName.trim()==='') {
+      setEnteredNameIsValid(false);
+    }
+  }
+
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
   const nameInputClasses = `form-control${!nameInputIsInvalid ? '' : ' invalid'}`;
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} onChange={nameChangeHandler} type='text' id='name' />
+        <input ref={nameInputRef} onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} type='text' id='name' />
         {nameInputIsInvalid && <p className='error-text'>Entered name is not valid</p>}
       </div>
       <div className="form-actions">
