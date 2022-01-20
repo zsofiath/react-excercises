20256e4539410a82c99db529a5abd6b88bde958f
zsofi <zsofiath@gmail.com>
2022-01-20 19:57:15 +0100

    :sparkles: insert more components

--- src/counter/App.js
+++ src/counter/App.js
@@ -1,11 +1,15 @@
 import Counter from "./components/Counter";
 import "./index.css";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 import { Provider } from "react-redux";
- import store from "./store/Store";
+import store from "./store/Store";
 
 function App() {
   return (
     <Provider store={store}>
+      <Header />
+      <Auth />
       <Counter />
     </Provider>
   );
