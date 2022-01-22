5c1e38ecb73288508b70247a3a79914b85a55ad8
zsofi <zsofiath@gmail.com>
2022-01-22 17:12:13 +0100

    :sparkles: show items

--- src/shop/components/Shop/Products.js
+++ src/shop/components/Shop/Products.js
@@ -1,16 +1,21 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+import { useSelector } from "react-redux";
 
 const Products = (props) => {
+  const itemList = useSelector((state) => state.itemList);
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {itemList.map((item) => (
+          <ProductItem
+            key={item.id}
+            title={item.name}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/shop/store/store.js
+++ src/shop/store/store.js
@@ -1,11 +1,20 @@
 import {createStore} from "redux";
 import { TOGGLE_CART } from "./ActionTypes";
 const initialState = {
-    isChartVisible: false
+    isChartVisible: false,
+    itemList: [
+        {id: 1, name: 'name 1', price: 2, description: 'Lorem ipsum'},
+        {id: 2, name: 'name 2', price: 6, description: 'Lorem ipsum'},
+        {id: 3, name: 'name 3', price: 3, description: 'Lorem ipsum'},
+        {id: 4, name: 'name 4', price: 77, description: 'Lorem ipsum'},
+    ]
 };
 
 const cartReducer = (state = initialState, action) => {
-    if(action.type === TOGGLE_CART) state = {isChartVisible: !state.isChartVisible}
+    if(action.type === TOGGLE_CART) state = {
+        itemList: state.itemList.filter(item => true),
+        isChartVisible: !state.isChartVisible
+    }
     return state;
 }
 
