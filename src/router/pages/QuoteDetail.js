50694ed45d4e090677636259f80dfcf0dbe0671f
zsofi <zsofiath@gmail.com>
2022-01-23 21:36:27 +0100

    :sparkles: add nested routes

--- src/router/pages/QuoteDetail.js
+++ src/router/pages/QuoteDetail.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {Route, Switch, useParams, Redirect} from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
 const Quotedetail = () => {
-    let param = useParams();
-    console.log(param);
-  return <div>Quotedetail {param.quoteId}</div>;
+  let param = useParams();
+  console.log(param);
+  return (
+    <>
+      <div>Quotedetail {param.quoteId}</div>
+      <Route path={`/quotes/${param.quoteId}/comments`}><Comments></Comments></Route>
+    </>
+  );
 };
 
 export default Quotedetail;
