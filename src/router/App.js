a769bd391adbc874bdc4aef0cdd6cd915c17d521
zsofi <zsofiath@gmail.com>
2022-01-23 21:27:15 +0100

    :sparkles: redirect

--- src/router/App.js
+++ src/router/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch, BrowserRouter} from "react-router-dom";
+import {Route, Switch, BrowserRouter, Redirect} from "react-router-dom";
 import Allquotes from "./pages/AllQuotes";
 import Quotedetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
@@ -6,6 +6,9 @@ function App() {
   return (
     <BrowserRouter>
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes"></Redirect>
+      </Route>
       <Route path="/quotes" exact><Allquotes></Allquotes></Route>
       <Route path="/new-quote"><NewQuote></NewQuote></Route>
       <Route path="/quotes/:quoteId"><Quotedetail></Quotedetail></Route>
