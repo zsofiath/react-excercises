3df3c452549ba3156c4ca2bb4a571beb50d7ba2c
zsofi <zsofiath@gmail.com>
2022-01-23 14:01:23 +0100

    :sparkles: try action creator

--- src/shop/components/Layout/MainHeader.js
+++ src/shop/components/Layout/MainHeader.js
@@ -1,10 +1,17 @@
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
+import {useSelector, useDispatch} from "react-redux";
+import { actionCreator } from '../../store/store';
 
 const MainHeader = (props) => {
+  const val = useSelector(state => state.Dummy.num);
+  const dispatch = useDispatch();
   return (
     <header className={classes.header}>
-      <h1>ReduxCart</h1>
+      <h1>ReduxCart {val}</h1>
+      <button onClick={() => {
+        dispatch(actionCreator());
+      }}>{val}</button>
       <nav>
         <ul>
           <li>
--- src/shop/store/store.js
+++ src/shop/store/store.js
@@ -12,20 +12,42 @@ const UISlice = createSlice({
   },
 });
 
+const dummySlice = createSlice({
+  name: "dummy",
+  initialState: {num:0},
+  reducers: {
+    set1: (state, action) => {
+      console.log(1, state, action.payload);
+      state.num = 1;
+    },
+    set2: (state, action) => {
+      console.log(2, state, action.payload);
+      state.num = 2;
+    },
+  },
+});
+
 const CartSlice = createSlice({
   name: "cart-slice",
-  initialState:  {items: [{ id: 1, title: "name 1", price: 2, quantity: 1, total: 3 }]},
+  initialState: {
+    items: [{ id: 1, title: "name 1", price: 2, quantity: 1, total: 3 }],
+  },
   reducers: {
     addItem: (state, action) => {
       const existingItem = state.items.filter((item) => {
         return item.id === action.payload.id;
       });
       if (existingItem.length == 0) {
-        const item2 = { ...action.payload, total: action.payload.price, quantity: 1 };
+        const item2 = {
+          ...action.payload,
+          total: action.payload.price,
+          quantity: 1,
+        };
         state.items.push(item2);
       } else {
         existingItem[0].quantity++;
-        existingItem[0].total = existingItem[0].quantity * existingItem[0].price;
+        existingItem[0].total =
+          existingItem[0].quantity * existingItem[0].price;
       }
     },
     removeItem: (state, action) => {
@@ -38,19 +60,33 @@ const CartSlice = createSlice({
         ];
       } else {
         existingItem[0].quantity--;
-        existingItem[0].total = existingItem[0].quantity * existingItem[0].price;
+        existingItem[0].total =
+          existingItem[0].quantity * existingItem[0].price;
       }
-    }
-  }
+    },
+  },
 });
 
 export const UIActions = UISlice.actions;
 export const CartActions = CartSlice.actions;
 
+export const actionCreator = (data) => {
+  console.log("Creator");
+  return (dispatch) => {
+    console.log("return funct");
+
+    dispatch(dummySlice.actions.set1(1));
+    setTimeout(() => {
+      dispatch(dummySlice.actions.set2(2));
+    }, 300);
+  };
+};
+
 const store = configureStore({
   reducer: {
+    Dummy: dummySlice.reducer,
     UI: UISlice.reducer,
-    Cart: CartSlice.reducer
+    Cart: CartSlice.reducer,
   },
 });
 
