3a482f933b57e83942bb3755ada21738686b36c7
zsofi <zsofiath@gmail.com>
2022-01-29 21:06:37 +0100

    :sparkles: add new quote form

--- src/router/pages/NewQuote.js
+++ src/router/pages/NewQuote.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const addQuoteHandler = (quote) => {
+        console.log(quote);
+    }
+
     return (
         <div>
-            NewQuote
+            <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>
         </div>
     );
 }
