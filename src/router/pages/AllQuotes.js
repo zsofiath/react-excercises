1149d8dde5f936f9ee344d2a08dab7c3fc89237a
zsofi <zsofiath@gmail.com>
2022-01-29 20:49:21 +0100

    :clown_face: add mock quotes

--- src/router/pages/AllQuotes.js
+++ src/router/pages/AllQuotes.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+    {id: 'id1', author: 'dsxc', text: '....'},
+    {id: 'id2', author: 'dsxc', text: '....'},
+    {id: 'id3', author: 'dsxc', text: '....'},
+    {id: 'id4', author: 'dsxc', text: '....'},
+    {id: 'id5', author: 'dsxc', text: '....'},
+];
 const Allquotes = () => {
     return (
-        <div>
-            Allquotes
-        </div>
+        <>
+        <QuoteList quotes={DUMMY_QUOTES}></QuoteList>
+        </>
     );
 }
 
