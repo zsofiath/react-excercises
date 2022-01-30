e8179137e754f90a94bb519be72053272d202a47
zsofi <zsofiath@gmail.com>
2022-01-30 11:32:26 +0100

    :sparkles: programatic navigation replace vs push

--- src/router/pages/NewQuote.js
+++ src/router/pages/NewQuote.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import QuoteForm from '../components/quotes/QuoteForm';
+import {useHistory} from "react-router-dom";
 
 const NewQuote = () => {
+    const history = useHistory();
     const addQuoteHandler = (quote) => {
         console.log(quote);
+        //history.replace('/quotes'); //cannot navigate back
+        history.push('/quotes'); //can navgate back
     }
 
     return (
