1c88a84aacfb45c767fadedfa5b2d9d118e7a80a
zsofi <zsofiath@gmail.com>
2022-01-30 13:50:46 +0100

    :recycle: route as object

--- src/router/components/quotes/QuoteList.js
+++ src/router/components/quotes/QuoteList.js
@@ -22,7 +22,10 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
   
   const changeSortingHandler = () => {
-    history.push(location.pathname+'?sort='+(isSortingAscending ? 'desc' : 'asc'));
+    history.push({
+      pathname: location.pathname,
+      search: '?sort='+(isSortingAscending ? 'desc' : 'asc')
+    });
   };
 
   return (
