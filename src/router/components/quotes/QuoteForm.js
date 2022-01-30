e01c569e1ed36faf5d10a3020dacf6dcdb1dd83c
zsofi <zsofiath@gmail.com>
2022-01-30 11:51:11 +0100

    :sparkles: prompt, preventing navigation is form touched prompt to prevent navigation

--- src/router/components/quotes/QuoteForm.js
+++ src/router/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import {Prompt} from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntering, setIsEntering] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,9 +21,22 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+
+  const finisEnteringHandler = () => {
+    // must be separated from submission because of the async behavior of setState.
+    // in submitHandler it would not be fast enough to allow navigation
+    setIsEntering(false);
+
+  }
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  }
+
   return (
+    <>
+    <Prompt when={isEntering} message={(location)=>'Are you sure you want to leave?'}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form className={classes.form} onSubmit={submitFormHandler} onFocus={formFocusHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +52,10 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button className='btn' onClick={finisEnteringHandler}>Add Quote</button>
         </div>
       </form>
-    </Card>
+    </Card></>
   );
 };
 
