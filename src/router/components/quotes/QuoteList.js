d9c2a6461936382388062399aec944e29141c72d
zsofi <zsofiath@gmail.com>
2022-01-30 12:40:42 +0100

    :sparkles: change sorting in url param

--- src/router/components/quotes/QuoteList.js
+++ src/router/components/quotes/QuoteList.js
@@ -1,11 +1,24 @@
-import { Fragment } from 'react';
-
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search); //js feature
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort='+(isSortingAscending ? 'desc' : 'asc'));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'descending' : 'ascending'}</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
